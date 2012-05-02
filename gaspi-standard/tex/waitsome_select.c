6b29194c5cd794027cde3a79e7576ca6476463de
Daniel Gruenewald <gruenewald@itwm.fraunhofer.de.com>
2012-05-02 17:37:26 +0000

    Included the modifications of Christian concerning the getter routines
    git-svn-id: https://fusionforge.zih.tu-dresden.de/svn/gaspi/trunk@8 ff55411a-4699-11e1-974c-f71ec6445003

--- gaspi-standard/tex/waitsome_select.c
+++ gaspi-standard/tex/waitsome_select.c
@@ -1,31 +1,32 @@
 #include <GASPI.h>
 #include <success_or_die.h>
 
-extern void process (const int flag, const gaspi_flag_t val);
+extern void process ( const gaspi_notification_id_t id
+                    , const gaspi_notification_t val );
 
-void waitsome ( const gaspi_flag_id_t flag_id_begin
-              , const gaspi_flag_id_t flag_num
+void waitsome ( const gaspi_notification_id_t id_begin
+              , const gaspi_notification_id_t id_num
               )
 {
 
   ASSERT ( gaspi_notify_waitsome
-           ( flag_id_begin
-           , flag_num
+           ( id_begin
+           , id_num
            , GASPI_BLOCK
            )
          );
 
   for (gaspi_flag_id_t i = 0; i < flag_num; ++i)
     {
-      gaspi_flag_t val = 0;
+      gaspi_notification_t notification = 0;
 
       // atomic reset
-      ASSERT (gaspi_notify_reset (flag_id_begin + i, &val));
+      ASSERT (gaspi_notify_reset (id_begin + i, &notification));
 
       // re-check, other threads are notified too!
       if (val != 0)
         {
-          process (i, val);
+          process (i, notification);
         }
         
     }
