64045a7d4930a5912a394440f0b35689febf8cd9
Christian Herold <christian.herold@tu-dresden.de>
2013-08-08 13:49:18 +0000

    extented script about slurm batch system
    git-svn-id: https://fusionforge.zih.tu-dresden.de/svn/gaspi/trunk@117 ff55411a-4699-11e1-974c-f71ec6445003

--- GPI2/gaspi_lsf_run.pl
+++ GPI2/gaspi_lsf_run.pl
@@ -4,11 +4,12 @@ use 5.10.0;
 use strict;
 
 my $lsf_pe_var    = "LSB_MCPU_HOSTS";
-my $machine_file  = "/home/cherold/GPI2-rc1/machinefile";
-my $gaspi_run     = "/home/cherold/GPI2-rc1/bin/gaspi_run";
+my $machine_file  = "/home/cherold/machinefile";
+my $gaspi_run     = "gaspi_run";
 my %host_cpu;
+my $master;
 
-sub generateMachineFile($)
+sub generateMachineFileLSF($)
 {
   my $FILE;
   my @pes = split(/ /, shift);
@@ -23,32 +24,63 @@ sub generateMachineFile($)
   }
   
   close($FILE);
+  return (sort(keys %host_cpu))[0];
 }
 
-if( scalar( @ARGV ) == 0 ) {
+sub generateMachineFileSLURM($)
+{
+  my $nodes = shift;
+  my $FILE;
+  
+  open( $FILE, ">$machine_file") or die "Can't open machinefile:\n $!";
+  
+  print $FILE "$nodes";
+  
+  close($FILE);
+  
+  return ( split(/\n/, $nodes) )[0] ;
+}
+
+if( scalar( @ARGV ) == 0 )
+{
   print "No arguments given !!\n";
   exit 1;
 }
 
-if (grep /--help/, @ARGV) {
+if (grep /--help/, @ARGV) 
+{
   print "#############################################################################\n";
   print "#                   Execution of an gaspi application:                      #\n";
-  print "#############################################################################\n\n";
-  print "<batch_system> [batchsystem_opt] /path/to/gaspi_run [gaspi_opt] ./application\n\n";
-  print "e.g.:\n";
-  print "   bsub -Is -n 2 -R \"span[hosts=1]\" ./gaspi_lsf_run.pl -n 2 ~/gpi2/bin/main\n\n";
   print "#############################################################################\n";
+  print "<batch_system> [batchsystem_opt] /path/to/gaspi_run [gaspi_opt] ./application\n\n";
+  print "#############################################################################\n";
+  print "#                            on atlas with lsf                              #\n";
+  print "#############################################################################\n\n";
+  print "   bsub -Is -n 2 -R \"span[hosts=1]\" ./gaspi_lsf_run.pl -n 2 /home/user/main\n\n";
+  print "#############################################################################\n";
+  print "#                            on taurus with slurm                           #\n";
+  print "#############################################################################\n\n";
+  print "         salloc -n 2 /home/user/gaspi_lsf_run.pl -n 2 /home/user/main\n\n";
+  print "#############################################################################\n\n";
+  #salloc -n 9 gaspi_lsf_run /home/cherold/gpi2/heat2d-gaspi/bin/heat2d-gaspi 30 30 3 3
   exit 1 ;
 }
 
-if( ! defined $ENV{$lsf_pe_var} ) {
+if( defined $ENV{$lsf_pe_var} ) 
+{
+  $master = generateMachineFileLSF( $ENV{$lsf_pe_var} );
+}
+elsif ( $ENV{"HOSTNAME"} =~ /taurus/ ) 
+{
+  $master = generateMachineFileSLURM( `srun hostname | sort` );
+}
+else 
+{
   print "Wrong batchsystem variable or no execution with batchsystem !\n";
   exit 1;
 }
 
-generateMachineFile( $ENV{$lsf_pe_var} );
-my $master = (sort(keys %host_cpu))[0];
 my $exec = "ssh ".$master." $gaspi_run -m $machine_file ";
 $exec .= join(' ', @ARGV);
-print "<< execution on master node ",(sort(keys %host_cpu))[0]," >>\n";
+print "<< execution on master node ",$master," >>\n";
 exec $exec;
