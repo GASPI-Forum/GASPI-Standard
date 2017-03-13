d11aaa386b49f75a6cc1f604ba831f8204a54c96
csimmend <christian.simmendinger@t-systems-sfr.com>
2017-03-13 14:05:55 +0100

    added broadcast

--- tutorials/default/code/solution/broadcast/constant.h
+++ tutorials/default/code/solution/broadcast/constant.h
@@ -2,10 +2,7 @@
 #ifndef CONSTANT_H
 #define CONSTANT_H
 
-
 #define MIN(x,y) ((x)<(y)?(x):(y))
-int const B_SZ = 2;
-int const M_SZ = 1024*1024;
-int const NWAY = 16;
+int const M_SZ = 128*128*128;
 
 #endif
